280b59aa730249afa08499ed2f80875190166d26
Salix8 <saulpachecotrilles@gmail>
2022-03-24 13:57:45 +0100

    acabando visualización verPais

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styleUrls: ['./pais-input.component.css']
+})
+export class PaisInputComponent implements OnInit {
+
+  @Output() onEnter   : EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+  
+  @Input() placeholder: string = ``;
+
+  debouncer: Subject<string> = new Subject();
+  termino  : string = ``;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(debounceTime(500))
+      .subscribe( valor => {
+        this.onDebounce.emit( valor );
+      })
+  }
+
+  buscar() {
+    this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada( event: any ){
+    // const valor = event.target.value;
+    // console.log(valor);
+    // console.log(this.termino); 
+    this.debouncer.next( this.termino );
+  }
+
+}
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from "../../interfaces/pais-interface";
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+  termino : string = `Esp`;
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar( termino: string){
+
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais( termino )
+    .subscribe( (paises) => {
+      console.log( paises );
+      this.paises = paises;
+
+    }, ( err ) => {
+      this.hayError = true;
+      this.paises = [];
+    });
+  }
+
+  sugerencias( termino: string){
+    this.hayError = false;
   }
 
 }
